0dcbf2a07670955e07d41a7100dc3e584d464c4e
Lars Gebraad <larsgebraad@pop-os.localdomain>
2019-10-08 01:24:53 +0200

    Added prior tests

--- hmc_tomography/Tests/test_mass_matrices.py
+++ hmc_tomography/Tests/test_mass_matrices.py
@@ -1,3 +1,5 @@
+"""A collection of tests for mass matrices.
+"""
 from hmc_tomography import MassMatrices as _MassMatrices
 import pytest as _pytest
 import numpy as _numpy
@@ -8,6 +10,18 @@ import numpy as _numpy
 @_pytest.mark.parametrize("dimensions", [1, 10, 100, 1000])
 def test_creation(mmclass: _MassMatrices._AbstractMassMatrix, dimensions: int):
     """Test for the creation of mass matrices.
+
+    Parameters
+    ==========
+    mmclass : hmc_tomography.MassMatrices._AbstractMassMatrix
+        A mass matrix class.
+    dimensions : int
+        Dimensions to check the mass matrix.
+
+
+    This test checks if we can create a given mass matrix. Using pytest, it will loop
+    over all available subclasses of the mass matrix, with variable amount of
+    dimensions.
     """
 
     # Create the object
@@ -35,6 +49,7 @@ def test_generate(mmclass: _MassMatrices._AbstractMassMatrix, dimensions: int):
     dimensions : int
         Dimensions to check the mass matrix.
 
+
     This test checks if we can generate momentum from a given mass matrix. Using pytest,
     it will loop over all available subclasses of the mass matrix, with variable amount
     of dimensions.
@@ -51,5 +66,74 @@ def test_generate(mmclass: _MassMatrices._AbstractMassMatrix, dimensions: int):
 
     # Assert float type
     assert momentum.dtype is _numpy.dtype("float")
+
+    return True
+
+
+@_pytest.mark.xfail(raises=NotImplementedError)
+@_pytest.mark.parametrize("mmclass", _MassMatrices._AbstractMassMatrix.__subclasses__())
+@_pytest.mark.parametrize("dimensions", [1, 10, 100, 1000])
+def test_kinetic_energy(mmclass: _MassMatrices._AbstractMassMatrix, dimensions: int):
+    """Test for the computation of kinetic energy for a given momentum.
+
+    Parameters
+    ==========
+    mmclass : hmc_tomography.MassMatrices._AbstractMassMatrix
+        A mass matrix class.
+    dimensions : int
+        Dimensions to check the mass matrix.
+
+
+    This test checks if we can compute kinetic energy for a given momentum. Using
+    pytest, it will loop over all available subclasses of the mass matrix, with
+    variable amount of dimensions.
+    """
+
+    # Create the object
+    mass_matrix: _MassMatrices._AbstractMassMatrix = mmclass(dimensions)
+
+    # Generate momentum
+    momentum = mass_matrix.generate_momentum()
+    kinetic_energy = mass_matrix.kinetic_energy(momentum)
+
+    # Assert float type
+    assert type(kinetic_energy) == float or type(kinetic_energy) == _numpy.float64
+
+    return True
+
+
+@_pytest.mark.xfail(raises=NotImplementedError)
+@_pytest.mark.parametrize("mmclass", _MassMatrices._AbstractMassMatrix.__subclasses__())
+@_pytest.mark.parametrize("dimensions", [1, 10, 100, 1000])
+def test_kinetic_energy_gradient(
+    mmclass: _MassMatrices._AbstractMassMatrix, dimensions: int
+):
+    """Test for the computation of kinetic energy gradient for a given momentum.
+
+    Parameters
+    ==========
+    mmclass : hmc_tomography.MassMatrices._AbstractMassMatrix
+        A mass matrix class.
+    dimensions : int
+        Dimensions to check the mass matrix.
+
+
+    This test checks if we can compute kinetic energy gradient for a given momentum.
+    Using pytest, it will loop over all available subclasses of the mass matrix, with
+    variable amount of dimensions.
+    """
+
+    # Create the object
+    mass_matrix: _MassMatrices._AbstractMassMatrix = mmclass(dimensions)
+
+    # Generate momentum
+    momentum = mass_matrix.generate_momentum()
+    kinetic_energy_gradient = mass_matrix.kinetic_energy_gradient(momentum)
+
+    # Assert column vector shape
+    assert kinetic_energy_gradient.shape == (dimensions, 1)
+
+    # Assert float type
+    assert kinetic_energy_gradient.dtype is _numpy.dtype("float")
 
     return True
