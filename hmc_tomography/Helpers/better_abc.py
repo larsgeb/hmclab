49ea292536287acddacfc363766b61fd102b5744
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2020-04-15 12:04:14 +0200

    Updated better abc to handle edge cases of LASIF

--- hmc_tomography/Helpers/better_abc.py
+++ hmc_tomography/Helpers/better_abc.py
@@ -29,11 +29,20 @@ def abstract_attribute(obj=None):
 class ABCMeta(NativeABCMeta):
     def __call__(cls, *args, **kwargs):
         instance = NativeABCMeta.__call__(cls, *args, **kwargs)
-        abstract_attributes = {
-            name
-            for name in dir(instance)
-            if getattr(getattr(instance, name), "__is_abstract_attribute__", False)
-        }
+
+        abstract_attributes = set()
+        for name in dir(instance):
+            attribute_to_check = getattr(instance, name)
+            try:
+                is_abstract = getattr(
+                    attribute_to_check, "__is_abstract_attribute__", False
+                )
+                if is_abstract:
+                    abstract_attribute.add(name)
+            except AttributeError:
+                # This is to handle LASIF's custom getattr methods.
+                pass
+
         if abstract_attributes:
             raise NotImplementedError(
                 "Can't instantiate abstract class {} with"
