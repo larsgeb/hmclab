d42fbb6427c7dbf11c956344975b0c5ba2588cf8
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2020-05-22 10:35:08 +0200

    Sped up source location, expanded on tests

--- hmc_tomography/Distributions/SourceLocation.py
+++ hmc_tomography/Distributions/SourceLocation.py
@@ -1,12 +1,14 @@
 """Earthquake source location inverse problems
 """
-import warnings as _warnings
 from typing import Union as _Union, Tuple as _Tuple
 
 import numpy as _numpy
 
-from hmc_tomography.Distributions import _AbstractDistribution, Normal
-from hmc_tomography.Helpers import RandomMatrices as _RandomMatrices
+from hmc_tomography.Distributions import _AbstractDistribution, Normal, Laplace
+
+# import line_profiler
+
+# profile = line_profiler.LineProfiler()
 
 
 class SourceLocation(_AbstractDistribution):
@@ -35,15 +37,36 @@ class SourceLocation(_AbstractDistribution):
         fixed_medium_velocity=None,
     ):
 
+        # Stations ---------------------------------------------------------------------
+
+        # Assert that the arrays are row vectors.
+        assert receiver_array_x.shape == (1, receiver_array_x.size)
+        assert receiver_array_z.shape == (1, receiver_array_z.size)
+
+        self.receiver_array_x = receiver_array_x
+        self.receiver_array_z = receiver_array_z
+
+        # Free parameters --------------------------------------------------------------
+
         # Determine if velocity is a free parameter
         self.infer_velocity = infer_velocity
         if not self.infer_velocity:
             assert fixed_medium_velocity is not None
             self.fixed_medium_velocity = fixed_medium_velocity
 
+        # Observed data and error statistics -------------------------------------------
+
         # Get number of events from data and array size
         assert observed_data.size % receiver_array_x.size == 0
-        self.number_of_events = int(observed_data.size / receiver_array_x.size)
+        self.number_of_events: int = int(observed_data.size / receiver_array_x.size)
+        self.number_of_stations: int = self.receiver_array_z.size
+        self.number_of_datums: int = self.number_of_events * self.number_of_stations
+
+        if observed_data.shape == (self.number_of_events, self.number_of_stations):
+            observed_data = observed_data.reshape(observed_data.size, 1)
+
+        if data_dispersion.shape == (self.number_of_events, self.number_of_stations):
+            data_dispersion = data_dispersion.reshape(data_dispersion.size, 1)
 
         # Assert that the data is a column vector
         assert observed_data.shape == (observed_data.size, 1)
@@ -57,13 +80,6 @@ class SourceLocation(_AbstractDistribution):
         else:
             raise ValueError("Misfit function is not implemented")
 
-        # Assert that the arrays are row vectors.
-        assert receiver_array_x.shape == (1, receiver_array_x.size)
-        assert receiver_array_z.shape == (1, receiver_array_z.size)
-
-        self.receiver_array_x = receiver_array_x
-        self.receiver_array_z = receiver_array_z
-
         self.dimensions: int = self.number_of_events * 3 + int(self.infer_velocity)
 
     def misfit(self, coordinates: _numpy.ndarray) -> float:
@@ -74,51 +90,40 @@ class SourceLocation(_AbstractDistribution):
 
         synthetic_data = SourceLocation.forward(
             x, z, T, v, self.receiver_array_x, self.receiver_array_z
-        )
+        ).reshape(self.number_of_datums, 1)
 
         return self.misfit_model.misfit(synthetic_data)
 
     def gradient(self, coordinates: _numpy.ndarray) -> _numpy.ndarray:
 
-        number_of_events = int(_numpy.floor(coordinates.size / 3))
-
         # Data gradient
         x, z, T, v = self.split_vector(coordinates, velocity=self.fixed_medium_velocity)
-        data = self.forward(x, z, T, v, self.receiver_array_x, self.receiver_array_z)
-        data_gradient = self.misfit_model.gradient(data)
+
+        data = self.forward(
+            x, z, T, v, self.receiver_array_x, self.receiver_array_z
+        ).reshape(self.number_of_datums, 1)
+
+        data_gradient = self.misfit_model.gradient(data).reshape(
+            self.number_of_events, self.number_of_stations
+        )
 
         # Forward model gradient
-        x, z, T, v = self.split_vector(coordinates, velocity=self.fixed_medium_velocity)
-        forward_gradient = self.forward_gradient(
+
+        dx, dz, dT, dv = self.forward_gradient(
             x, z, T, v, self.receiver_array_x, self.receiver_array_z
         )
-
-        number_of_stations = self.receiver_array_z.size
-
-        total_gradient = forward_gradient * data_gradient
-
-        total_gradient_x_sum = _numpy.sum(
-            total_gradient[:, 0].reshape(number_of_events, number_of_stations), axis=1
-        )
-        total_gradient_z_sum = _numpy.sum(
-            total_gradient[:, 1].reshape(number_of_events, number_of_stations), axis=1
-        )
-        total_gradient_T_sum = _numpy.sum(
-            total_gradient[:, 2].reshape(number_of_events, number_of_stations), axis=1
-        )
-        total_gradient_v_sum = _numpy.sum(total_gradient[:, 3])
 
         total_gradient = _numpy.empty((self.dimensions, 1))
 
         if self.infer_velocity:
-            total_gradient[0:-1:3] = total_gradient_x_sum[:, None]
-            total_gradient[1:-1:3] = total_gradient_z_sum[:, None]
-            total_gradient[2:-1:3] = total_gradient_T_sum[:, None]
-            total_gradient[-1] = total_gradient_v_sum
+            total_gradient[0:-1:3, 0] = _numpy.sum(data_gradient * dx, axis=1)
+            total_gradient[1:-1:3, 0] = _numpy.sum(data_gradient * dz, axis=1)
+            total_gradient[2:-1:3, 0] = _numpy.sum(data_gradient * dT, axis=1)
+            total_gradient[-1, 0] = _numpy.sum(data_gradient * dT)
         else:
-            total_gradient[0::3] = total_gradient_x_sum[:, None]
-            total_gradient[1::3] = total_gradient_z_sum[:, None]
-            total_gradient[2::3] = total_gradient_T_sum[:, None]
+            total_gradient[0::3, 0] = _numpy.sum(data_gradient * dx, axis=1)
+            total_gradient[1::3, 0] = _numpy.sum(data_gradient * dz, axis=1)
+            total_gradient[2::3, 0] = _numpy.sum(data_gradient * dT, axis=1)
 
         return total_gradient
 
@@ -130,15 +135,15 @@ class SourceLocation(_AbstractDistribution):
     @staticmethod
     def forward(x, z, T, v, receiver_array_x, receiver_array_z) -> _numpy.ndarray:
 
-        # Assert that the vectors are column-shaped
-        assert x.shape == (x.size, 1)
-        assert z.shape == (z.size, 1)
-        assert T.shape == (T.size, 1)
+        # # Assert that the vectors are column-shaped
+        # assert x.shape == (x.size, 1)
+        # assert z.shape == (z.size, 1)
+        # assert T.shape == (T.size, 1)
         # Assert that the vectors are the same length
-        assert x.size == z.size and x.size == T.size
+        # assert x.size == z.size and x.size == T.size
 
-        assert receiver_array_x.shape == receiver_array_z.shape
-        assert receiver_array_x.shape == (1, receiver_array_x.size)
+        # assert receiver_array_x.shape == receiver_array_z.shape
+        # assert receiver_array_x.shape == (1, receiver_array_x.size)
 
         # Forward calculate the travel time by dividing distance (Pythagoras) by speed,
         # and adding origin time.
@@ -159,9 +164,9 @@ class SourceLocation(_AbstractDistribution):
         )
 
         # Subsequently we flatten the array to make it compatible with the distributions
-        # to use it with distributions for errors.
+        # to use it with distributions for errors. !<DISABLED FOR NOW>!
 
-        return traveltimes.reshape(traveltimes.size, 1)
+        return traveltimes  # .reshape(traveltimes.size, 1)
 
     @staticmethod
     def forward_gradient(
@@ -169,14 +174,14 @@ class SourceLocation(_AbstractDistribution):
     ) -> _numpy.ndarray:
 
         # Assert that the vectors are column-shaped
-        assert x.shape == (x.size, 1)
-        assert z.shape == (z.size, 1)
-        assert T.shape == (T.size, 1)
+        # assert x.shape == (x.size, 1)
+        # assert z.shape == (z.size, 1)
+        # assert T.shape == (T.size, 1)
         # Assert that the vectors are the same length
-        assert x.size == z.size and x.size == T.size
+        # assert x.size == z.size and x.size == T.size
 
-        assert receiver_array_x.shape == receiver_array_z.shape
-        assert receiver_array_x.shape == (1, receiver_array_x.size)
+        # assert receiver_array_x.shape == receiver_array_z.shape
+        # assert receiver_array_x.shape == (1, receiver_array_x.size)
 
         # Forward calculate the travel time by dividing distance (Pythagoras) by speed,
         # and adding origin time.
@@ -195,16 +200,12 @@ class SourceLocation(_AbstractDistribution):
             (x - receiver_array_x) ** 2.0 + (z - receiver_array_z) ** 2.0
         ) ** 0.5
 
-        gradient_x = ((x - receiver_array_x) / (v * distances)).reshape(
-            distances.size, 1
-        )
-        gradient_z = ((z - receiver_array_z) / (v * distances)).reshape(
-            distances.size, 1
-        )
-        gradient_T = (_numpy.ones_like(gradient_x)).reshape(distances.size, 1)
-        gradient_v = (-distances / (v * v)).reshape(distances.size, 1)
+        gradient_x = (x - receiver_array_x) / (v * distances)
+        gradient_z = (z - receiver_array_z) / (v * distances)
+        gradient_T = 0.0 * gradient_x + 1.0
+        gradient_v = -distances / (v * v)
 
-        return _numpy.hstack((gradient_x, gradient_z, gradient_T, gradient_v))
+        return gradient_x, gradient_z, gradient_T, gradient_v
 
     @staticmethod
     def create_default(dimensions):
--- hmc_tomography/Tests/test_source_location.py
+++ hmc_tomography/Tests/test_source_location.py
@@ -1,59 +1,73 @@
 import hmc_tomography as _hmc_tomography
 import numpy as _numpy
 import numpy.matlib
+import os as _os
 
+from line_profiler import LineProfiler
 
 SourceLocation = _hmc_tomography.Distributions.SourceLocation
 
+profile = LineProfiler()
+profile.add_function(_hmc_tomography.Distributions.SourceLocation.gradient)
+profile.add_function(_hmc_tomography.Distributions.SourceLocation.forward)
+profile.add_function(_hmc_tomography.Distributions.SourceLocation.forward_gradient)
 
+
+@profile
 def test_basic():
 
     stations_x = _numpy.array([-10.0, 5.0, 10.0])[None, :]
     stations_z = _numpy.array([0.0, 0.0, 0.0])[None, :]
 
-    # x, z, T, v
+    # Create the true model and observations -------------------------------------------
+
     true_model = _numpy.array([0, 10, 0, 0, 12, 0, 2.5])[:, None]
 
     x, z, T, v = SourceLocation.split_vector(true_model)
 
     fake_observed_data = SourceLocation.forward(x, z, T, v, stations_x, stations_z)
-    data_dispersion = _numpy.ones_like(fake_observed_data)
+
+    # Create the likelihood ------------------------------------------------------------
+
+    data_dispersion = 0.25 * _numpy.ones_like(fake_observed_data)
 
     likelihood = SourceLocation(
         stations_x, stations_z, fake_observed_data, data_dispersion, infer_velocity=True
     )
 
-    print()
-    print(likelihood.gradient(true_model))
-
-    samples_filename = "samples_source_location.h5"
-
-    # Creating the prior
+    # Creating the prior ---------------------------------------------------------------
 
     prior_event = _hmc_tomography.Distributions.Uniform([-20, 0, 0], [20, 25, 100])
 
-    prior_velocity = _hmc_tomography.Distributions.Normal(2.5, 0.5)
+    prior_velocity = _hmc_tomography.Distributions.Normal(2.5, 2.5)
 
     composite_prior = _hmc_tomography.Distributions.CompositeDistribution(
         [prior_event, prior_event, prior_velocity]
     )
 
+    # Applying Bayes rule --------------------------------------------------------------
+
     posterior = _hmc_tomography.Distributions.BayesRule([composite_prior, likelihood])
 
-    step_size = 0.005 * _numpy.matlib.repmat(
-        prior_event.upper_bounds - prior_event.lower_bounds, 2, 1
-    )
-    step_size = _numpy.append(step_size, [[0.25]], axis=0)
+    # Sampling -------------------------------------------------------------------------
 
-    _hmc_tomography.Samplers.HMC.sample(
-        samples_filename,
+    filename = "temporary_file.h5"
+
+    # Remove file before attempting to sample
+    if _os.path.exists(filename):
+        _os.remove(filename)
+
+    _hmc_tomography.Samplers.RWMH.sample(
+        filename,
         posterior,
-        time_step=0.1,
-        amount_of_steps=100,
+        # mass_matrix=mm,
+        # time_step=0.005,
+        # amount_of_steps=50,
         initial_model=true_model,
         proposals=1000,
+        step_length=0.1,
+        online_thinning=1,
         overwrite_existing_file=True,
     )
 
-    with _hmc_tomography.Post.Samples(samples_filename) as samples:
-        _hmc_tomography.Post.Visualization.marginal_grid(samples, [0, 1, 2, 3, 4, 5, 6])
+    _os.remove(filename)
