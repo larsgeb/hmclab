Eliminated dtype bug Fixed a case where the type of data covariance cannot be safely cast to the type of the other objects (i.e. double to float).

hmc_tomography/Distributions/LinearMatrix.py
@@ -6,6 +6,7 @@ from typing import Union as _Union
 import numpy as _numpy
 import scipy as _scipy
 import scipy.sparse as _sparse
+from scipy.sparse import data
 import scipy.sparse.linalg as _sparse_linalg
 
 from hmc_tomography.Distributions import _AbstractDistribution
@@ -22,9 +23,16 @@ class LinearMatrix(_AbstractDistribution):
         G: _Union[_numpy.ndarray, _sparse.spmatrix],
         d: _numpy.ndarray,
         data_covariance: _Union[float, _numpy.ndarray, _sparse.spmatrix],
-        dtype=_numpy.single,
+        dtype=None,
         **kwargs,
     ):
+
+        # Set precision for inconsistently passed objects ------------------------------
+        if dtype is not None:
+            G.dtype = dtype
+        d = d.astype(G.dtype)
+        if type(data_covariance) not in [float, _numpy.float32, _numpy.float64]:
+            data_covariance = data_covariance.astype(G.dtype)
 
         # Four cases:
         # 1 - Dense G, scalar/vector covariance
@@ -367,7 +375,7 @@ class _LinearMatrix_sparse_forward_simple_covariance(_AbstractDistribution):
 
                 except OSError:
                     _warnings.warn(
-                        f"MKL not found, will evaluate matrix-vector products using "
+                        "MKL not found, will evaluate matrix-vector products using "
                         "SciPy.",
                         Warning,
                     )
@@ -449,7 +457,7 @@ class _LinearMatrix_sparse_forward_sparse_covariance(_AbstractDistribution):
         self.data_covariance = data_covariance
 
         self.Gt = self.G.T.tocsr()
-        self.dt = d.T
+        self.dt = d.T.astype(dtype)
         self.factorized_covariance = _sparse_linalg.factorized(
             self.data_covariance.tocsc()
         )
@@ -459,12 +467,16 @@ class _LinearMatrix_sparse_forward_sparse_covariance(_AbstractDistribution):
             0.5
             * (
                 (coordinates.T @ self.Gt - self.dt)
-                @ self.factorized_covariance((self.G @ coordinates - self.d))
+                @ self.factorized_covariance(
+                    (self.G @ coordinates - self.d).astype(self.data_covariance.dtype)
+                )
             ).item()
         )
 
     def gradient(self, coordinates: _numpy.ndarray) -> _numpy.ndarray:
-        return self.Gt @ self.factorized_covariance(self.G @ coordinates - self.d)
+        return self.Gt @ self.factorized_covariance(
+            (self.G @ coordinates - self.d).astype(self.data_covariance.dtype)
+        )
 
     def generate(self) -> _numpy.ndarray:
         raise NotImplementedError()