8c32c9b82c8ed4a3ad346bc569c74ceb3b0efbf2
Lars Gebraad <lars.gebraad@erdw.ethz.ch>
2022-03-08 17:40:37 +0100

    More pickling tests

--- tests/test_pickling.py
+++ tests/test_pickling.py
@@ -1,19 +1,22 @@
-"""A collection of tests for likelihood functions.
-"""
-import matplotlib.pyplot as _plt
-import numpy as _numpy
 import pytest as _pytest
 import dill as _dill
-
 from hmclab import Distributions as _Distributions
 from hmclab.Helpers.CustomExceptions import InvalidCaseError as _InvalidCaseError
+import numpy as _numpy
+import pytest as _pytest
+import hmclab as _hmclab
+
+_ad = _hmclab.Distributions._AbstractDistribution
+_as = _hmclab.Samplers._AbstractSampler
+
 
 dimensions = [1, 2, 5, 50]
-subclasses = _Distributions._AbstractDistribution.__subclasses__()
-deltas = [1e-10, 1e-2, -1e-10, -1e-2]
+distribution_classes = _ad.__subclasses__()
+sampler_classes = _as.__subclasses__()
+sampler_classes.remove(_hmclab.Samplers._AbstractVisualSampler)
 
 
-@_pytest.mark.parametrize("pclass", subclasses)
+@_pytest.mark.parametrize("pclass", distribution_classes)
 @_pytest.mark.parametrize("dimensions", dimensions)
 def test_creation(pclass: _Distributions._AbstractDistribution, dimensions: int):
     # Create the object
@@ -34,4 +37,20 @@ def test_creation(pclass: _Distributions._AbstractDistribution, dimensions: int)
         assert _dill.pickles(distribution)
     except AssertionError as e:
         print(_dill.detect.badtypes(distribution, depth=1))
+        raise e
+
+
+@_pytest.mark.parametrize("sampler_class", sampler_classes)
+def test_basic_sampling(
+    sampler_class: _as,
+):
+
+    sampler_instance = sampler_class()
+
+    assert isinstance(sampler_instance, _as)
+
+    try:
+        assert _dill.pickles(sampler_instance)
+    except AssertionError as e:
+        print(_dill.detect.badtypes(sampler_instance, depth=1))
         raise e
